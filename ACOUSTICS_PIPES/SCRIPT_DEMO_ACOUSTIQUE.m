d5b8a2271189e3a6506c6d5d7b480e87520a15aa
erbafdavid <david.fabre@imft.fr>
2018-03-07 14:03:00 +0100

    fix

--- ACOUSTICS_PIPES/SCRIPT_DEMO_ACOUSTIQUE.m
+++ ACOUSTICS_PIPES/SCRIPT_DEMO_ACOUSTIQUE.m
@@ -14,25 +14,48 @@ bf = SF_Init('Mesh_1.edp');
 AC = SF_Acoustic(bf,'k',0.1)
 
 % trac? de la structure du champ acoustique
-figure(1);
 plotFF(AC,'Phi');
+pause(0.1);
+
 % pour tracer Phi*r on peut faire : 
 % AC.R = sqrt(AC.mesh.X.^2+AC.mesh.Y.^2);
 % AC.PhiR = AC.Phi*AC.R;
 % plotFF(AC,'PhiR')
 
+% Pour tracer u et p sur l'axe :
+  figure(2);
+  plot(AC.Xaxis,real(AC.Paxis),'r-',AC.Xaxis,imag(AC.Paxis),'r--',...
+               AC.Xaxis,real(AC.Uaxis),'b-',AC.Xaxis,imag(AC.Uaxis),'b--');
+           title('Pressure and velocity along the axis ')
+           legend('Re(P)', 'Im(P)','Re(U)','Im(U)'); 
+  pause(0.1);
+
+% Remarque : on peut directement faire le calcul et les trac?s avec 
+% la commande suivante :
+% AC = SF_Acoustic(bf,'k',0.1,'plotPhi','yes','plotaxis','yes')
+
 % Calcul de l'impedance pour plusieurs valeurs de k
-figure(2);
-IMP = SF_Acoustic_Impedance(bf,'k',[0:.1:2])
+
+IMP = SF_Acoustic_Impedance(bf,'k',[0:.01:2])
 
 % trace de Z(k) parties reelles et imaginaires
 figure(3);
 plot(IMP.k,real(IMP.Z),'b',IMP.k,imag(IMP.Z),'b--');
+title('Impedance : real and imaginary parts')
+xlabel('k'); ylabel('Z_r,Z_i')
+pause(0.1);
+
 % trace de |Z(k)| en semilog
 figure(4);
 semilogy(IMP.k,abs(IMP.Z));
+pause(0.1);
+title('Impedance : absolute value, semilog plot')
+xlabel('k'); ylabel('|Z|')
+
 % trace de R(k)
-%semilogy(IMP.k,IMP.R);
+semilogy(IMP.k,IMP.R);
+title('Reflexion coefficient')
+xlabel('k'); ylabel('R')
 
 
 
