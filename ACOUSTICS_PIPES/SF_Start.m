27c5f7b3b76489fd330cd76ac949e3cbf17b551b
erbafdavid <david.fabre@imft.fr>
2018-03-08 17:53:03 +0100

    add path for windows systems

--- ACOUSTICS_PIPES/SF_Start.m
+++ ACOUSTICS_PIPES/SF_Start.m
@@ -2,6 +2,8 @@ global ff ffdir ffdatadir sfdir verbosity
 %ff = '/PRODCOM/FREEFEM/Ubuntu12.04/3.29/bin/FreeFem++-nw'; % on IMFT network
 %ff = '/usr/local/ff++/openmpi-2.1/3.55/bin/FreeFem++-nw'; % on DF's macbookpro 
 ff =  '/usr/local/bin/FreeFem++' % on the server AZTECA (UPS)
+ff = 'launchff++'; % for windows systems
+
 ffdatadir = './';
 %sfdir = '/Users/dfabre/StabFem/SOURCES_MATLAB/'; % where to find the matlab drivers
 sfdir = './';
@@ -11,4 +13,5 @@ verbosity = 5;
 %if(exist(ffdatadir)~=7)
 %    mysystem(['mkdir ' ffdatadir]);
 %end
-mysystem(['echo "// File automatically created by StabFem" > SF_Geom.edp']); % a file SF_Geom should be present, even if blank 
+mysystem(['echo "// File automatically created by StabFem" > SF_Geom.edp']); 
+% a file SF_Geom should be present, even if blank 
