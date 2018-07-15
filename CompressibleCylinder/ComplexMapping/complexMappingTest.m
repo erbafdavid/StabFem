7143e1a68023bfcb78128a296206f5e960c9b5e9
jsagg <jsagg93@gmail.com>
2018-07-15 10:02:52 +0200

    complexMethodtest updated and working

--- CompressibleCylinder/ComplexMapping/complexMappingTest.m
+++ CompressibleCylinder/ComplexMapping/complexMappingTest.m
@@ -43,13 +43,16 @@ Rec = 49.15
 
 bf = SF_Init('Mesh_Cylinder.edp',[xinfm,xinfv,yinf]);
 bf=SF_BaseFlow(bf,'Re',10,'Mach',Ma,'ncores',1,'type','NEW');
-bf=SF_Adapt(bf,'Hmax',6);
-bf=SF_BaseFlow(bf,'Re',60,'Mach',Ma,'ncores',1,'type','NEW');
-bf=SF_Adapt(bf,'Hmax',6);
-[ev,em] = SF_Stability(bf,'shift',0.02844+ 0.71*i,'nev',1,'type','D','sym','A','Ma',Ma);
-[bf,em]=SF_Adapt(bf,em,'Hmax',6);
+bf=SF_Adapt(bf,'Hmax',1);
 bf=SF_BaseFlow(bf,'Re',Rec,'Mach',Ma,'ncores',1,'type','NEW');
+bf=SF_Adapt(bf,'Hmax',1);
 [ev,em] = SF_Stability(bf,'shift',+ Omegac*i,'nev',1,'type','D','sym','A','Ma',Ma);
+[bf,em]=SF_Adapt(bf,em,'Hmax',3);
+
+
+
+bf=SF_BaseFlow(bf,'Re',Rec,'Mach',Ma,'ncores',1,'type','NEW');
+[ev,em] = SF_Stability(bf,'shift',+ Omegac*i,'nev',1,'type','S','sym','A','Ma',Ma);
 
 
 figure();
@@ -57,7 +60,17 @@ figure();
 bf.xlim = [-1.5 4.5]; bf.ylim=[0,3];
 plotFF(bf,'ux','Contour','on','Levels',[0 0]);
 %plotFF(bf,'ux');
-title('Base flow with complex mapping');
+str = 'Box $$B_3$$ Baseflow without complex mapping';
+title(str,'Interpreter','latex');
+box on; pos = get(gcf,'Position'); pos(4)=pos(3)*AspectRatio;set(gcf,'Position',pos); % resize aspect ratio
+set(gca,'FontSize', 18);
+
+figure();
+% plot the eigenmode for Re = Rec
+em.xlim = [-2 8]; em.ylim=[0,5];
+plotFF(em,'ux1Adj');
+str = ' Adjoint Eigenmode with $$\gamma_c = 1.0$$';
+title(str,'Interpreter','latex');
 box on; pos = get(gcf,'Position'); pos(4)=pos(3)*AspectRatio;set(gcf,'Position',pos); % resize aspect ratio
 set(gca,'FontSize', 18);
 
@@ -65,14 +78,15 @@ figure();
 % plot the eigenmode for Re = Rec
 em.xlim = [-2 8]; em.ylim=[0,5];
 plotFF(em,'ux1');
-title('Eigenmode with complex mapping');
+str = 'Eigenmode with $$\gamma_c = 0.0$$';
+title(str,'Interpreter','latex');
 box on; pos = get(gcf,'Position'); pos(4)=pos(3)*AspectRatio;set(gcf,'Position',pos); % resize aspect ratio
 set(gca,'FontSize', 18);
 
 % plot the mesh (full size)
 figure()
 plotFF(bf,'mesh');
-title('Initial mesh (full size)');
-box on; %pos = get(gcf,'Position'); pos(4)=pos(3)*AspectRatio;set(gcf,'Position',pos); % resize aspect ratio
+str = 'Refined mesh';
+title(str,'Interpreter','latex');box on; %pos = get(gcf,'Position'); pos(4)=pos(3)*AspectRatio;set(gcf,'Position',pos); % resize aspect ratio
 set(gca,'FontSize', 18);
 saveas(gca,'Cylinder_Mesh_Full',figureformat); 
