9cce959314e1241def10b0ee5e452e800737582b
jsagg <jsagg93@gmail.com>
2018-09-28 00:47:54 +0200

    Bug corrected in WhilstlingHole SLEPc

--- CASES_DEVELOPMENT/WhistlingHole/TESTS_Eigenvalues.m
+++ CASES_DEVELOPMENT/WhistlingHole/TESTS_Eigenvalues.m
@@ -33,10 +33,21 @@ evG1 = SF_Stability(bf,'shift',-2.1i,'m',0,'nev',20)
 % SECOND test : USING solver  Stab_Axi_COMPLEX_m0.edp adapted from Raffaele's sources
 % note that this solver is switched on by the "trick" m = 0+1i...
 % nev = 20 => Slepc solver
-ev = SF_Stability(bf,'shift',-2.1i,'m',0+1i,'nev',20)
+ev = SF_Stability(bf,'shift',-2.1i,'m',0,'nev',20)
 % nev = 1 => shift-invert solver
 ev1 = SF_Stability(bf,'shift',-2.1i,'m',0,'nev',20)
 
-plot(evG,evG1,ev,ev1)
+scatter(real(evG), imag(evG),'MarkerEdgeColor',[0 .5 .5],...
+              'MarkerFaceColor',[0 .7 .7],...
+              'LineWidth',1.5)
+hold on;
+scatter(real(evG1),imag(evG1),'s','red',...
+              'LineWidth',1.5)
+
+scatter(real(ev),imag(ev),'s','magenta',...
+              'LineWidth',1.5)
+hold on;
+scatter(real(ev1),imag(ev1),'s','black',...
+              'LineWidth',1.5)
 legend('Stab_Axi_Complex.edp, nev=20','Stab_Axi_Complex.edp, nev=1','Stab_Axi_Complex_m0.edp, nev=20','Stab_Axi_Complex_m0.edp, nev=1');
 
