b80c33f13bf0ad371d73f36ac1df55d9d2901f70
jsagg <jsagg93@gmail.com>
2018-10-08 13:42:22 +0200

    StabAxi_COMPLEX_m0.edp bugs corrected. Wrong definition of nu based on the Reynolds numbers and wrong declaration of OP and B operators for SLEPc usage

--- DEVELOPMENT_CASES/WhistlingHole/SmartMesh_Hole.m
+++ DEVELOPMENT_CASES/WhistlingHole/SmartMesh_Hole.m
@@ -9,18 +9,19 @@ Params = [0 17 2.5 0. 5 17]; % Lm, LA, LC, gammac, yA, yB
 
 bf = SF_BaseFlow(bf,'Re',1,'MappingParams', Params);
 bf = SF_BaseFlow(bf,'Re',10,'MappingParams', Params);
-bf = SF_Adapt(bf);
+bf = SF_Adapt(bf,'Hmax',0.25);
 bf = SF_BaseFlow(bf,'Re',30,'MappingParams', Params);
 bf = SF_BaseFlow(bf,'Re',100,'MappingParams', Params);
-bf = SF_Adapt(bf); 
+bf = SF_Adapt(bf,'Hmax',0.25); 
 Params = [0 17 2.5 0.1 5 17];
 bf = SF_BaseFlow(bf,'Re',300,'MappingParams', Params);
-bf = SF_Adapt(bf);
+bf = SF_Adapt(bf,'Hmax',0.25);
 Params = [0 17 2.5 0.3 5 17];
 bf = SF_BaseFlow(bf,'Re',350,'MappingParams', Params);
 bf = SF_BaseFlow(bf,'Re',1000,'MappingParams', Params);
-bf = SF_Adapt(bf);
-
+bf = SF_Adapt(bf,'Hmax',0.25);
+bf = SF_BaseFlow(bf,'Re',1500,'MappingParams', Params);
+bf = SF_Adapt(bf,'Hmax',0.25);
 %bf = SF_BaseFlow(bf,'Re',1500,'MappingParams', Params);
 %bf = SF_Adapt(bf);
 
@@ -31,6 +32,6 @@ ForcedFlow1 = SF_ForcedFlow(bf,Omega1);
 Omega2 = 4;
 ForcedFlow2 = SF_ForcedFlow(bf,Omega2);
 %figure; plotFF(ForcedFlow2,'ux1');
-bf = SF_Adapt(bf,ForcedFlow1,ForcedFlow2);
+bf = SF_Adapt(bf,ForcedFlow1,ForcedFlow2,'Hmax',0.25);
  
 end
