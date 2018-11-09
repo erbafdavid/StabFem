7f89b7b742c0af133c20a6ab555867878a487475
David Fabre <erbafdavid@gmail.com>
2018-11-09 08:45:03 +0100

    Changing names of plotting functions : plotFF -> SF_Plot (and plotFF_ETA -> SF_Plot_ETA)

---
+++ SOURCES_MATLAB/SF_Plot.m
@@ -0,0 +1,261 @@
+function handle = plotFF(FFdata, varargin);
+%  function plotFF
+%  plots a data field imported from freefem.
+%  This function is part of the StabFem project by D. Fabre & coworkers.
+%
+%  Usage :
+%  1/ handle=plotFF(mesh); (if mesh is a mesh object)
+%  2/ handle=plotFF(ffdata,'field'); to plot isocontours of a P1 field
+%  3/ handle=plotFF(ffdata,'field.re'); to plot isocontours of a P1 complex
+%  field (specify '.re' or '.im')
+%  4/ handle=plotFF(ffdata,'field',[PARAM,VALUE,..])
+%      where [PARAM,VALUE] is any couple of parameters accepted by ffpdeplot.
+%       (See Below)
+%
+%  FFdata is the structure containing the data to plot
+%  'field' is the field to plot (the data may comprise multiple fields)
+%
+%   This version of plotFF is based on ffpdeplot developed by chloros2
+%   as an Octave-compatible alternative to pdeplot from toolbox pdetools
+%   (https://github.com/samplemaker/freefem_matlab_octave_plot)
+%
+%   [PARAM,VALUE] are any couple of name/value parameter accepted by
+%   ffpdeplot. The list of possibilities currently comprises the following :
+%
+%   specifies parameter name/value pairs to control the input file format
+%
+%       Parameter       Value
+%      'XYData'      Data in order to colorize the plot
+%                       FreeFem++ point data | FreeFem++ triangle data
+%      'XYStyle'     Coloring choice
+%                       'interp' (default) | 'off'
+%      'ZStyle'      Draws 3D surface plot instead of flat 2D Map plot
+%                       'continuous' | 'off' (default)
+%      'ColorMap'    ColorMap value or matrix of such values
+%                       'cool' (default) | colormap name | three-column matrix of RGB triplets
+%      'ColorBar'    Indicator in order to include a colorbar
+%                       'on' (default) | 'off' | 'northoutside' ...
+%      'CBTitle'     Colorbar Title
+%                       (default=[])
+%      'ColorRange'  Range of values to adjust the color thresholds
+%                       'minmax' (default) | 'centered' | 'cropminmax' | 'cropcentered' | [min,max]
+%      'Mesh'        Switches the mesh off / on
+%                       'on' | 'off' (default)
+%      'Boundary'    Shows the domain boundary / edges
+%                       'on' | 'off' (default)
+%      'BDLabels'    Draws boundary / edges with a specific label
+%                       [] (default) | [label1,label2,...]
+%      'BDColors'    Colorize boundary / edges with color (linked to 'BDLabels')
+%                       'r' (default) | ['r','g', ... ]
+%      'BDShowText'  Shows the labelnumber on the boundary / edges
+%                       'on' | 'off' (default)
+%      'Contour'     Isovalue plot
+%                       'off' (default) | 'on'
+%      'CXYData'     Use extra (overlay) data to draw the contour plot
+%                       FreeFem++ points | FreeFem++ triangle data
+%      'CStyle'      Contour plot style
+%                       'patch' (default) | 'patchdashed' | 'patchdashedneg' | 'monochrome' | 'colormap'
+%      'CColor'      Isovalue color
+%                       [0,0,0] (default) | RGB triplet | 'r' | 'g' | 'b' |
+%      'CLevels'     Number of isovalues used in the contour plot
+%                       (default=10)
+%      'CGridParam'  Number of grid points used for the contour plot
+%                       'auto' (default) | [N,M]
+%      'Title'       Title
+%                       (default=[])
+%      'XLim'        Range for the x-axis
+%                       'minmax' (default) | [min,max]
+%      'YLim'        Range for the y-axis
+%                       'minmax' (default) | [min,max]
+%      'ZLim'        Range for the z-axis
+%                       'minmax' (default) | [min,max]
+%      'DAspect'     Data unit length of the xy- and z-axes
+%                       'off' | 'xyequal' (default) | [ux,uy,uz]
+%      'FlowData'    Data for quiver plot
+%                       FreeFem++ point data | FreeFem++ triangle data
+%      'FGridParam'  Number of grid points used for quiver plot
+%                       'auto' (default) | [N,M]
+%
+%       'symmetry'  symmetry property of the flow to plot
+%                       'no' (default) | 'YS' (symmetric w.r.t. Y axis) | 'YA' (antisymmetric w.r.t. Y axis) | 'XS' | 'XA' 
+%                                      | 'XM' (mirror image w/r to X axis) | 'YM'      
+%
+%     Notes :
+
+
+global ff ffdir ffdatadir sfdir verbosity
+
+
+% first check if 'colormap' is a custom one 
+%   (a few customs are defined at the bottom of this function)
+for i=1:nargin-2
+    if(strcmp(lower(varargin{i}),'colormap'))
+        switch(lower(varargin{i+1}))
+            case('redblue')
+                varargin{i+1} = redblue(); % defined at the bottom
+            case('french')
+                varargin{i+1} = french();
+            case('ice')
+                varargin{i+1} = ice();
+                %otherwise varargin{i+1} should be a standard colormap
+        end    
+    end   
+end
+
+% check if 'contour' is part of the parameters and recovers its value
+contourval='off';
+for i=1:nargin-2
+      if(strcmp(varargin{i},'contour'))
+           icontour = i;
+           contourval = varargin{i+1};
+      end    
+end
+
+% check if 'symmetry' is part of the parameters and recovers it
+symmetry = 'no';
+for i=1:nargin-2
+      if(strcmp(varargin{i},'symmetry'))
+           isymmetry = i;
+           symmetry = varargin{i+1};
+      end    
+end
+if (strcmp(symmetry,'no')~=1)
+       varargin = { varargin{1:isymmetry-1} ,varargin{isymmetry+2:end}} ;
+end
+
+
+
+
+
+
+%%% prepares to invoke ffpdeplot...
+
+if (mod(nargin, 2) == 1) % plot mesh in single-entry mode : mesh
+    mesh = FFdata;
+    varargin = {varargin{:}, 'mesh', 'on'};
+    if(isfield(mesh,'xlim'))
+        varargin = { varargin{:}, 'xlim', mesh.xlim};
+    end
+    if(isfield(mesh,'ylim'))
+        varargin = { varargin{:}, 'ylim', mesh.ylim};
+    end
+    mydisp(15, ['launching ffpeplot with the following options :']);
+    if (verbosity >= 15)
+        varargin;
+    end;
+    handle = ffpdeplot(mesh.points, mesh.bounds, mesh.tri, varargin{:});
+else % plot mesh in single-entry mode : data
+    mesh = FFdata.mesh;
+    field1 = varargin{1};
+    varargin = {varargin{2:end}};
+     if(isfield(mesh,'xlim'))
+        varargin = {varargin{:}, 'xlim', mesh.xlim};
+    end
+    if(isfield(mesh,'ylim'))
+        varargin = {varargin{:}, 'ylim', mesh.ylim};
+    end
+    if (strcmp(field1, 'mesh')) % plot mesh ins double-entry mde
+        varargin = {varargin{:}, 'mesh', 'on'};
+        
+        mydisp(15, ['launching ffpeplot with the following options :']);
+        if (verbosity >= 15)
+            varargin
+        end;
+          
+        handle = ffpdeplot(mesh.points, mesh.bounds, mesh.tri, varargin{:});
+        %axis equal;
+    else
+        % plot data
+        
+        % check if data to plot is the name of a field or a numerical dataset
+        if (~isnumeric(field1))
+            [dumb, field, suffix] = fileparts(field1); % to extract the suffix
+            if (strcmp(suffix, '.im') == 1)
+                data = imag(getfield(FFdata, field));
+            else
+                data = real(getfield(FFdata, field));
+            end
+        else
+            data = field1;
+        end
+        
+        
+        if (strcmpi(contourval,'off')~=1&&strcmpi(contourval,'on')~=1)
+        varargin{icontour} = 'on';
+        [dumb, field, suffix] = fileparts(contourval); % to extract the suffix
+            if (strcmp(suffix, '.im') == 1)
+                xydata = imag(getfield(FFdata, field));
+            else
+                data = real(getfield(FFdata, field));
+            end
+        varargin = { varargin{:} , 'cxydata',xydata } ;
+        end
+
+        mydisp(20, ['launching ffpeplot with the following options :']);
+        if (verbosity >= 20)
+            varargin
+        end;
+          
+ pointsS = FFdata.mesh.points;
+ if(strcmpi(symmetry,'xm'))
+        pointsS(2,:) = -pointsS(2,:);
+        symmetry = 'no';
+ elseif(strcmpi(symmetry,'ym'))
+        pointsS(1,:) = -pointsS(1,:); 
+        symmetry ='no';
+  end
+
+  handle = ffpdeplot(pointsS, FFdata.mesh.bounds, FFdata.mesh.tri, 'xydata', data, varargin{:});
+
+    
+%%% SYMMETRIZATION OF THE PLOT
+if(strcmp(symmetry,'no'))
+        mydisp(20,'No symmetry');
+else   
+     mydisp(20,['Symmetrizing the plot with option ',symmetry]);
+  pointsS = FFdata.mesh.points;
+  switch(symmetry)
+    case('XS')
+        pointsS(2,:) = -pointsS(2,:);dataS = data;
+    case('XA')
+       pointsS(2,:) = -pointsS(2,:);dataS = -data;
+    case('YS')
+        pointsS(1,:) = -pointsS(1,:);dataS = data;
+    case('YA')
+        pointsS(1,:) = -pointsS(1,:);dataS = -data;
+    case({'XM','YM'})
+          % do nothing as these case has already been treated
+      otherwise
+        error(' Error in plotFF with option symmetry ; value must be XS,XA,YS,YA,XM,YM or no')      
+  end
+  
+    hold on;
+    handle = ffpdeplot(pointsS, FFdata.mesh.bounds, FFdata.mesh.tri, 'xydata', dataS, varargin{:});
+    hold off;
+end
+
+end
+
+end
+
+end
+
+% custom colormaps
+function cmap = redblue()
+colmapdef=[193,0,0; 235,164,164; 235,235,235; 196,196,255; 127,127,255]/255;
+[sz1,~]=size(colmapdef);
+cmap=interp1(linspace(0,1,sz1),colmapdef,linspace(0,1,255));
+end
+
+function cmap = french()
+colmapdef=[255,0,0; 255,255,255; 0,0,255]/255;
+[sz1,~]=size(colmapdef);
+cmap=interp1(linspace(0,1,sz1),colmapdef,linspace(0,1,255));
+end
+
+function cmap = ice()
+%definition of the colormap "ice"
+colmapdef=[255,255,255; 125,255,255; 0,123,255; 0,0,124; 0,0,0]/255;
+[sz1,~]=size(colmapdef);
+cmap=interp1(linspace(0,1,sz1),colmapdef,linspace(0,1,255));
+end
--- SOURCES_MATLAB/plotFF_ETA.m
+++ SOURCES_MATLAB/plotFF_ETA.m
@@ -1,52 +1,12 @@
-function h = plotFF_ETA(eigenmode,varargin)
+function h = plotFF_ETA(varargin)
+%
+% This functions adds to the current plot a representation of the
+% free-surface displacement ETA.
+%
+% Usage (just after SF_Plot(em,'ux')
+% SF_Plot(em,['E',E,'style',color,'LineWidth',lw,'projection','n'|'r'|'z?,symmmetry,'sym')
 
-p = inputParser;
-addParameter(p,'Amp',.15,@isnumeric);
-addParameter(p,'style','r',@ischar); % style for plots (e.g. color)
-addParameter(p,'LineWidth',2,@isnum); % Linewidth
-addParameter(p,'projection','n',@ischar); % projection : 'n' | 'r' | 'z'
-addParameter(p,'symmetry','no',@ischar); % symmetry condition. 
-                                         % available values are 'no', 
-                                         % 'YS' (symmetric with respect to Y axis)
-                                         % 'YA' (antisymmetric with respect to Y axis) 
-parse(p,varargin{:});
-E = p.Results.Amp;
-
-ffmesh = eigenmode.mesh;
-
-switch(p.Results.projection)
-    case('n')
-    h = plot(ffmesh.xsurf+real(E*eigenmode.eta).*ffmesh.N0r,ffmesh.ysurf+real(E*eigenmode.eta).*ffmesh.N0z,p.Results.style,'LineWidth',p.Results.LineWidth);
-    case('r')
-    h = plot(ffmesh.xsurf+real(E*eigenmode.eta)./ffmesh.N0r,p.Results.style,'LineWidth',p.Results.LineWidth);
-    case('z')
-    h = plot(ffmesh.xsurf,ffmesh.ysurf+real(E*eigenmode.eta)./ffmesh.N0z,p.Results.style,'LineWidth',p.Results.LineWidth);
-end
-        
-
-
-switch p.Results.symmetry
-    case('no')
-        mydisp(15,'No symmetry');
-    case('YS')
-        eigenmodeSYM = eigenmode;
-        eigenmodeSYM.mesh.xsurf = - eigenmodeSYM.mesh.xsurf;
-        hold on; 
-        plotFF_ETA(eigenmodeSYM,varargin{:},'symmetry','no');
-        %
-        %hold on; 
-        %h1 = plot(-ffmesh.xsurf-real(E*eigenmode.eta).*ffmesh.N0r,ffmesh.ysurf+real(E*eigenmode.eta).*ffmesh.N0z,p.Results.style,'LineWidth',p.Results.LineWidth);
-        %h = [h; h1];
-    case('YA')
-        %hold on; 
-        %h1 = plot(-ffmesh.xsurf+real(E*eigenmode.eta).*ffmesh.N0r,ffmesh.ysurf-real(E*eigenmode.eta).*ffmesh.N0z,p.Results.style,'LineWidth',p.Results.LineWidth);
-        %h = [h; h1];
-        eigenmodeSYM = eigenmode;
-        eigenmodeSYM.mesh.xsurf = - eigenmodeSYM.mesh.xsurf;
-        eigenmodeSYM.eta = - eigenmodeSYM.eta;
-        hold on; 
-        plotFF_ETA(eigenmodeSYM,varargin{:},'symmetry','no');
-end
-
+disp(' WARNING : function plotFF_ETA is now called SF_Plot_ETA. Please update your programs !') 
+handle = SF_Plot_ETA(varargin{:});
 
 end
