58a11eb5fe19b5b9a1ff870f0cf2f89a8c31fb9b
jsagg <jsagg93@gmail.com>
2018-11-21 23:10:11 +0100

    spectrum script

--- DEVELOPMENT_CASES/BirdCallCOMPLEX/spectrum.m
+++ DEVELOPMENT_CASES/BirdCallCOMPLEX/spectrum.m
@@ -1,42 +1,54 @@
-%% Computation of the spectrum
-
 run('../../SOURCES_MATLAB/SF_Start.m');
 figureformat='png'; AspectRatio = 0.56; % for figures
 tinit = tic;
 verbosity=20;
 Ma = 0.05;
 
-meshRef = 1.0;
-LA = 200;
-Params =[20 20 LA 35 0.2 15 LA 35 0.2]
+if(exist('bf','var'))
+    ffmesh = importFFmesh('../BirdCallComplex/MESH30x30DIRECT/MESHES/mesh_adapt10_Re1600_Ma0.05.msh');
+    bf = importFFdata(ffmesh,'../BirdCallComplex/MESH30x30DIRECT/MESHES/BaseFlow_adapt10_Re1600_Ma0.05.ff2m');
+else
+    bf = smartmesh('Ma',0.05,'MeshRefine',1,'RefineType','S');
+end
 
-ffmesh = importFFmesh('./WORK/MESHES/mesh_adapt12_Re1600_Ma0.05.msh')
-bf = importFFdata(ffmesh,'./WORK/BASEFLOWS/BaseFlow_adapt12_Re1600_Ma0.05.ff2m')
+Re = 320;
+bf=SF_BaseFlow(bf,'Re',Re,'Mach',Ma,'ncores',1,'type','NEW');
 
-spectrum = [];
-spectrumStruct = [];
-Imag_Range = [2:0.5:10];
+%% COMPUTATION OF THE SPECTRA WITHOUT COMPLEX MAPPING
+
+spectrumNOCM = [];
+spectrumStructNOCM = [];
+Imag_Range = [0.5:0.5:10];
 Real_Range = [1.0:-0.1:-0.5];
 for realTerm = Real_Range
     for imagTerm = Imag_Range
         evSearch = realTerm - imagTerm*1i;
-        [ev,em] = SF_Stability(bf,'shift',evSearch,'m',0,'nev',10,'type','D')
-        spectrum = [spectrum, ev ];
-        spectrumStruct = [spectrumStruct, em];
+        [ev,em] = SF_Stability(bf, 'shift', evSearch, 'm', 0, 'nev', 10, 'type', 'D')
+        spectrumNOCM = [spectrum, ev];
+        spectrumStructNOCM = [spectrumStructNOCM, em];
     end
 end
 
-Params =[10000 1000000 LA 35 0.0 10000 LA 100000 0.0];
-bf=SF_BaseFlow(bf,'Re',1600,'Mach',Ma,'ncores',1,'type','NEW','MappingParams',Params);
-spectrumNoCM = [];
-spectrumStructNoCM = [];
-Imag_Range = [2:0.5:10];
+%% COMPUTATION OF THE SPECTRA WITH COMPLEX MAPPING
+LA = 50;
+LAy = 50;
+Params =[10 10 LA 10.0 0.1 10.0 LAy 10.0 0.1]
+bf = SF_SetMapping(bf,'mappingtype','box','mappingparams',Params);
+bf=SF_BaseFlow(bf,'Re',Re,'Mach',Ma,'ncores',1,'type','NEW');
+Params =[10 10 LA 10.0 0.15 10.0 LAy 10.0 0.15]
+bf = SF_SetMapping(bf,'mappingtype','box','mappingparams',Params);
+bf=SF_BaseFlow(bf,'Re',Re,'Mach',Ma,'ncores',1,'type','NEW');
+
+
+spectrum = [];
+spectrumStruct = [];
+Imag_Range = [0.5:0.5:10];
 Real_Range = [1.0:-0.1:-0.5];
 for realTerm = Real_Range
     for imagTerm = Imag_Range
         evSearch = realTerm - imagTerm*1i;
-        [ev,em] = SF_Stability(bf,'shift',evSearch,'m',0,'nev',10,'type','D')
-        spectrumNoCM = [spectrumNoCM, ev ];
-        spectrumStructNoCM = [spectrumStructNoCM, em];
+        [ev,em] = SF_Stability(bf, 'shift', evSearch, 'm', 0, 'nev', 10, 'type', 'D')
+        spectrum = [spectrum, ev];
+        spectrumStruct = [spectrumStruct, em];
     end
 end
